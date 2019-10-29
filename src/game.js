77fb34ba97ae21017f0980ed6d140a9e3ea9eb4d
Jack <brook278@msu.edu>
2019-10-29 17:54:45 -0400

    Hi

--- src/game.js
+++ src/game.js
@@ -2,146 +2,31 @@ import {parse_json} from './parse_json';
 import $ from 'jquery';
 export const Game = function(sel) {
     this.sel = sel;
-    var that = this;
     var board_pieces = $(sel + " td button");
     for (var i = 0; i < board_pieces.length; ++i) {
         var board_piece = $(board_pieces.get(i));
-        this.installButtonListener(board_piece);
+        this.installBoardListener(board_piece);
     }
+
     var control = $(sel + " .controls");
     var check_button = control.find('button[name="check"]');
-    check_button.click(function (event) {
-        event.preventDefault();
-        $.ajax({
-            url: "post/game-post.php",
-            data: {check: true},
-            method: "POST",
-            success: function (data) {
-                var json = parse_json(data);
-                if (json.check) {
-                    var sel = that.sel;
-                    var new_table = $(sel + " table");
-                    that.message("<p>Successfully updated</p>");
-                    new_table.html(json.view);
-                    new Game(sel);
-                } else {
-                    //Error
-                    that.message("<p>Update failed</p>");
-                }
-            },
-            error: function (xhr, status, error) {
-                // Error
-                that.message("<p>Error: " + error + "</p>");
-            }
-        });
-    });
-    var clear_button = control.find('button[name="clear"]');
-    clear_button.click(function (event) {
-        event.preventDefault();
-        $.ajax({
-            url: "post/game-post.php",
-            data: {clear: true},
-            method: "POST",
-            success: function (data) {
-                var json = parse_json(data);
-                if (json.clear) {
-                    var sel = that.sel;
-                    var game = $(sel + ".game");
-                    that.message("<p>Successfully updated</p>");
-                    game.html(json.view);
-                    new Game(sel);
-                } else {
-                    //Error
-                    that.message("<p>Update failed</p>");
-                }
-            },
-            error: function (xhr, status, error) {
-                // Error
-                that.message("<p>Error: " + error + "</p>");
-            }
-        });
-    });
-    var solve_button = control.find('button[name="solve"]');
-    solve_button.click(function (event) {
-        event.preventDefault();
-        $.ajax({
-            url: "post/game-post.php",
-            data: {solve: true},
-            method: "POST",
-            success: function (data) {
-                var json = parse_json(data);
-                if (json.solve) {
-                    var sel = that.sel;
-                    var game = $(sel + ".game");
-                    that.message("<p>Successfully updated</p>");
-                    game.html(json.view);
-                    new Game(sel);
-                } else {
-                    //Error
-                    that.message("<p>Update failed</p>");
-                }
-            },
-            error: function (xhr, status, error) {
-                // Error
-                that.message("<p>Error: " + error + "</p>");
-            }
-        });
-    });
-    var yes_button = control.find('button[name="yes"]');
-    yes_button.click(function (event) {
-        event.preventDefault();
-        $.ajax({
-            url: "post/game-post.php",
-            data: {yes: true},
-            method: "POST",
-            success: function (data) {
-                var json = parse_json(data);
-                if (json.yes) {
-                    var sel = that.sel;
-                    var game = $(sel + ".game");
-                    that.message("<p>Successfully updated</p>");
-                    game.html(json.view);
-                    new Game(sel);
-                } else {
-                    //Error
-                    that.message("<p>Update failed</p>");
-                }
-            },
-            error: function (xhr, status, error) {
-                // Error
-                that.message("<p>Error: " + error + "</p>");
-            }
-        });
-    });
-    var no_button = control.find('button[name="no"]');
-    no_button.click(function (event) {
-        event.preventDefault();
-        $.ajax({
-            url: "post/game-post.php",
-            data: {no: true},
-            method: "POST",
-            success: function (data) {
-                var json = parse_json(data);
-                if (json.no) {
-                    var sel = that.sel;
-                    var game = $(sel + ".game");
-                    that.message("<p>Successfully updated</p>");
-                    game.html(json.view);
-                    new Game(sel);
-                } else {
-                    //Error
-                    that.message("<p>Update failed</p>");
-                }
-            },
-            error: function (xhr, status, error) {
-                // Error
-                that.message("<p>Error: " + error + "</p>");
-            }
-        });
-    });
-}
+    this.installButtonListener(this.sel, check_button, {check: true}, "check");
 
-Game.prototype.installButtonListener = function(board_piece) {
+    var clear_button = control.find('button[name="clear"]');
+    this.installButtonListener(this.sel, clear_button, {clear: true}, "clear");
+
+    var solve_button = control.find('button[name="solve"]');
+    this.installButtonListener(this.sel, solve_button, {solve: true}, "solve");
+
+    var no_button = control.find('button[name="no"]');
+    this.installButtonListener(this.sel, no_button, {no: true}, "no");
+
+    var yes_button = control.find('button[name="yes"]');
+    this.installButtonListener(this.sel, yes_button, {yes: true}, "yes");
+
+};
+
+Game.prototype.installBoardListener = function(board_piece) {
     var that = this;
     board_piece.click(function (event) {
 
@@ -174,11 +59,42 @@ Game.prototype.installButtonListener = function(board_piece) {
             }
         });
     });
-}
+};
+
+Game.prototype.installButtonListener = function(sel, button, data, data_name) {
+    var that = this;
+    button.click(function (event) {
+        event.preventDefault();
+        $.ajax({
+            url: "post/game-post.php",
+            data: data,
+            method: "POST",
+            success: function (data) {
+                var json = parse_json(data);
+                if (json.hasOwnProperty(data_name)) {
+                    var sel = that.sel;
+                    var game = $(sel);
+                    if (data_name === "check") {
+                        game = $(sel + " table");
+                    }
+                    that.message("<p>Successfully updated</p>");
+                    game.html(json.view);
+                    new Game(sel);
+                } else {
+                    //Error
+                    that.message("<p>Update failed</p>");
+                }
+            },
+            error: function (xhr, status, error) {
+                // Error
+                that.message("<p>Error: " + error + "</p>");
+            }
+        });
+    });
+};
 
 Game.prototype.message = function(message) {
     var that = this;
     $(that.sel + " .message").html(message);
-    $(that.sel + " .message").fadeIn(1000);
-    $(that.sel + " .message").show().delay(2000).fadeOut(1000);
-}
+    $(that.sel + " .message").html(message).hide();
+};
